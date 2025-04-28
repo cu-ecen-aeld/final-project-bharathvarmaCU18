fcfd1cb64de8624681944bc30939a2b90f179107
Bharath Varma Gowribidanur Chinnappa <bhgo7033@colorado.edu>
2025-04-27 23:51:11 -0600

    Font additions in QT application

--- yocto-tegra/meta-custom/recipes-qt/qt-hello-world/files/main.cpp
+++ yocto-tegra/meta-custom/recipes-qt/qt-hello-world/files/main.cpp
@@ -1,11 +1,34 @@
 #include <QApplication>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 #include <QLabel>
+#include <QFontDatabase> 
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel label("<center>Hello World from Qt5 on Jetson Nano!</center>");
-    label.resize(400, 150);
-    label.show();
+    
+
+    int id = QFontDatabase::addApplicationFont("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont font(family);
+    app.setFont(font);
+    
+    QWidget window;
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    
+    QPushButton *btn1 = new QPushButton("Test Button 1");
+    QPushButton *btn2 = new QPushButton("Test Button 2");
+    QLabel *label = new QLabel("Qt Widgets Test");
+    
+    layout->addWidget(label);
+    layout->addWidget(btn1);
+    layout->addWidget(btn2);
+    
+    window.setLayout(layout);
+    window.resize(400, 300);
+    window.show();
+    
     return app.exec();
 }
